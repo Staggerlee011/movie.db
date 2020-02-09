0e2c2daada6b3389a551594dee44c9a793a931d9
Stephen Bennett <stebennettsjb@gmail.com>
2020-02-09 20:21:15 +0000

    clean up of v2

--- sql/V2__populate.sql
+++ sql/V2__populate.sql
@@ -1,12 +1,13 @@
-INSERT directors (directorsName) 
-    VALUES  ('Chistopher Nolan'),
-            ('James Camron');
+INSERT INTO directors (directorsId, directorsName) 
+    OVERRIDING SYSTEM VALUE  
+    VALUES  (1, 'Chistopher Nolan'),
+            (2, 'James Camron');
 
 
-INSERT movies (moviesName, directorId, releaseYear) 
+INSERT INTO movies (moviesName, directorsId, releaseYear)
+    OVERRIDING SYSTEM VALUE  
     VALUES  ('Dunkirk', 1, 2017),
             ('Interstellar', 1, 2014),
             ('Avatar', 2, 2009),
             ('Titanic', 2, 1997),
             ('The Terminator', 2, 1984);
-
